a8b91c3050ae53bd4f8fbb78def70c204f59608e
Mahiuddin Al Kamal <mahiuddinalkamal@gmail.com>
2026-01-01 20:57:30 +0100

    Clean cached data on startup (#273)

---
+++ src/main/java/com/kirjaswappi/backend/common/components/RedisStartupCleaner.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2026 KirjaSwappi or KirjaSwappi affiliate company. All rights reserved.
+ * Author: Mahiuddin Al Kamal <mahiuddinalkamal>
+ */
+package com.kirjaswappi.backend.common.components;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("cloud")
+public class RedisStartupCleaner implements ApplicationRunner {
+
+  private static final Logger logger = LoggerFactory.getLogger(RedisStartupCleaner.class);
+  private final RedisConnectionFactory redisConnectionFactory;
+
+  public RedisStartupCleaner(RedisConnectionFactory redisConnectionFactory) {
+    this.redisConnectionFactory = redisConnectionFactory;
+  }
+
+  @Override
+  public void run(ApplicationArguments args) throws Exception {
+    try (var connection = redisConnectionFactory.getConnection()) {
+      connection.serverCommands().flushDb();
+      logger.info("Redis cache cleared successfully on startup.");
+    } catch (Exception e) {
+      logger.error("Failed to clear Redis cache on startup", e);
+    }
+  }
+}
